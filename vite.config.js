bf792b22aa60a8dcdba0f0dc8a63465ec5b9901f
samuel-durand <samuel.durand@laplateforme.io>
2025-11-21 14:55:52 +0100

    Restauration complÃ¨te: remplacement par les fichiers du dossier client de main

--- vite.config.js
+++ vite.config.js
@@ -26,8 +26,8 @@ export default defineConfig({
     },
   },
   preview: {
-    host: '0.0.0.0',
     port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
+    host: '0.0.0.0',
     strictPort: false,
     allowedHosts: [
       'cinego-front.up.railway.app',
